1ec7251f547c5ac7d4224b1ff69468c836bac872
Thijmen Petersen <t.petersen@arcady.nl>
2024-01-12 11:26:29 +0100

    linting

--- src/public/test-db.php
+++ src/public/test-db.php
@@ -4,11 +4,10 @@ $username = "user";
 $password = "secret";
 
 try {
-// Create connection
-$conn = new PDO("mysql:host=$servername;dbname=MyDb", $username, $password);
+  // Create connection
+  $conn = new PDO("mysql:host=$servername;dbname=MyDb", $username, $password);
 
-echo "Connected successfully";
-} catch(PDOException $e) {
+  echo "Connected successfully";
+} catch (PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
-?>
